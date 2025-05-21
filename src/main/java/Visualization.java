42017f510ed94e61e4334ed8f62f501917506215
Nazarenee <cph-cm425@cphbusiness.dk>
2025-05-21 10:47:49 +0200

    improved visualistion

--- src/main/java/Visualization.java
+++ src/main/java/Visualization.java
@@ -4,24 +4,29 @@ public class Visualization extends PApplet {
 
     Room livingRoom;
     Room kitchen;
+    Room bathroom;
 
-    // Buttons info (position and size)
     int btnWidth = 120;
     int btnHeight = 30;
 
-    // Button positions for Living Room
     int livingOnX, livingOnY;
     int livingOffX, livingOffY;
 
-    // Button positions for Kitchen
     int kitchenOnX, kitchenOnY;
     int kitchenOffX, kitchenOffY;
 
-    // Commands
+    int bathroomOnX, bathroomOnY;
+    int bathroomOffX, bathroomOffY;
+
+    int moodyX, moodyY;
+
     Command livingTurnOnCommand;
     Command livingTurnOffCommand;
     Command kitchenTurnOnCommand;
     Command kitchenTurnOffCommand;
+    Command bathroomTurnOnCommand;
+    Command bathroomTurnOffCommand;
+    MacroCommand moodyModeCommand;
 
     public static void main(String[] args) {
         PApplet.main("Visualization");
@@ -29,7 +34,7 @@ public class Visualization extends PApplet {
 
     @Override
     public void settings() {
-        size(900, 500);
+        size(900, 900);
     }
 
     @Override
@@ -38,17 +43,21 @@ public class Visualization extends PApplet {
 
         livingRoom = new Room("Living Room");
         kitchen = new Room("Kitchen");
+        bathroom = new Room("Bathroom");
 
         Light livingMain = new Light("Main Light", 255, 255, 200, 80, 120);
         Light livingAccent = new Light("Accent Light", 100, 200, 255, 200, 170);
         Light kitchenLight = new Light("Kitchen Light", 255, 255, 255, 120, 120);
+        Light bathroomLight = new Light("Bathroom Light", 0, 0, 255, 140, 120);
 
         livingRoom.addDevice(livingMain);
         livingRoom.addDevice(livingAccent);
         kitchen.addDevice(kitchenLight);
+        bathroom.addDevice(bathroomLight);
 
         livingMain.turnOn();
         kitchenLight.turnOn();
+        bathroomLight.turnOn();
 
         noStroke();
         textAlign(CENTER, CENTER);
@@ -65,10 +74,38 @@ public class Visualization extends PApplet {
         kitchenOffX = 470 + 180;
         kitchenOffY = kitchenOnY;
 
+        bathroomOnX = 250 + 50;
+        bathroomOnY = 450 + 250;
+        bathroomOffX = 250 + 180;
+        bathroomOffY = bathroomOnY;
+
+        moodyX = 360;
+        moodyY = 800;
+
         livingTurnOnCommand = new TurnOnAllCommand(livingRoom);
         livingTurnOffCommand = new TurnOffAllCommand(livingRoom);
         kitchenTurnOnCommand = new TurnOnAllCommand(kitchen);
         kitchenTurnOffCommand = new TurnOffAllCommand(kitchen);
+        bathroomTurnOnCommand = new TurnOnAllCommand(bathroom);
+        bathroomTurnOffCommand = new TurnOffAllCommand(bathroom);
+
+        moodyModeCommand = new MacroCommand("Moody Mode");
+
+
+        for (Device d : kitchen.getDevices()) {
+            moodyModeCommand.addCommand(new TurnOnCommand(d));
+        }
+        for (Device d : bathroom.getDevices()) {
+            moodyModeCommand.addCommand(new TurnOffCommand(d));
+        }
+
+        for (Device d : livingRoom.getDevices()) {
+            if (d.getName().equals("Main Light")) {
+                moodyModeCommand.addCommand(new TurnOffCommand(d));
+            } else if (d.getName().equals("Accent Light")) {
+                moodyModeCommand.addCommand(new TurnOnCommand(d));
+            }
+        }
     }
 
     @Override
@@ -77,14 +114,22 @@ public class Visualization extends PApplet {
 
         drawRoomWithShadow(40, 50, 350, 350, livingRoom.getName());
         drawRoomWithShadow(470, 50, 350, 350, kitchen.getName());
+        drawRoomWithShadow(250, 450, 350, 300, bathroom.getName());
 
         visualizeRoom(livingRoom, 40, 50);
         visualizeRoom(kitchen, 470, 50);
+        visualizeRoom(bathroom, 250, 450);
 
         drawButton(livingOnX, livingOnY, btnWidth, btnHeight, "Turn ON All");
         drawButton(livingOffX, livingOffY, btnWidth, btnHeight, "Turn OFF All");
+
         drawButton(kitchenOnX, kitchenOnY, btnWidth, btnHeight, "Turn ON All");
         drawButton(kitchenOffX, kitchenOffY, btnWidth, btnHeight, "Turn OFF All");
+
+        drawButton(bathroomOnX, bathroomOnY, btnWidth, btnHeight, "Turn ON All");
+        drawButton(bathroomOffX, bathroomOffY, btnWidth, btnHeight, "Turn OFF All");
+
+        drawButton(moodyX, moodyY, btnWidth + 40, btnHeight, "Moody Mode");
     }
 
     private void drawRoomWithShadow(int x, int y, int w, int h, String roomName) {
@@ -106,11 +151,9 @@ public class Visualization extends PApplet {
                 int x = offsetX + light.getXPosition();
                 int y = offsetY + light.getYPosition();
                 int r = light.getRadius();
-                int red = light.getRed();
-                int green = light.getGreen();
-                int blue = light.getBlue();
 
-                fill(red, green, blue);
+
+                fill(light.getRed(), light.getGreen(), light.getBlue());
                 stroke(255);
                 ellipse(x, y, r, r);
                 fill(255);
@@ -119,7 +162,6 @@ public class Visualization extends PApplet {
             }
         }
     }
-
 
     private void drawButton(int x, int y, int w, int h, String label) {
         fill(100);
@@ -131,7 +173,6 @@ public class Visualization extends PApplet {
 
     @Override
     public void mousePressed() {
-
         if (isMouseOverButton(livingOnX, livingOnY, btnWidth, btnHeight)) {
             livingTurnOnCommand.execute();
         } else if (isMouseOverButton(livingOffX, livingOffY, btnWidth, btnHeight)) {
@@ -140,6 +181,12 @@ public class Visualization extends PApplet {
             kitchenTurnOnCommand.execute();
         } else if (isMouseOverButton(kitchenOffX, kitchenOffY, btnWidth, btnHeight)) {
             kitchenTurnOffCommand.execute();
+        } else if (isMouseOverButton(bathroomOnX, bathroomOnY, btnWidth, btnHeight)) {
+            bathroomTurnOnCommand.execute();
+        } else if (isMouseOverButton(bathroomOffX, bathroomOffY, btnWidth, btnHeight)) {
+            bathroomTurnOffCommand.execute();
+        } else if (isMouseOverButton(moodyX, moodyY, btnWidth + 40, btnHeight)) {
+            moodyModeCommand.execute();
         }
     }
 
